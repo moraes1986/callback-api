42ef3e16f0c70829c45ac35ba12b6c4e26390e96
Thiago Moraes <thiago.moraes@luizalabs.com>
2024-12-31 10:57:30 -0300

    update requests

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import logging
 
 app = Flask(__name__)
 
-# Definindo um endpoint para o webhook
-@app.route('/webhook')
+# Definindo um endpoint para o webhook para receber o callback
+@app.route('/webhook', methods=['POST, GET'])
 def webhook():
     # Obtendo os dados enviados no corpo da requisição
     data = request.get_json()
@@ -14,6 +14,12 @@ def webhook():
 
     # Respondendo com status 200 e um JSON de confirmação
     return jsonify({"message": "Webhook recebido com sucesso!"}), 200
+
+@app.route('/callback')
+def index():
+    print('args:', request.args)
+
+    return request.args.get('data', 'none')
 
 if __name__ == '__main__':
     # Rodando o servidor na porta 5000
